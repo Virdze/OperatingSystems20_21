ff4bc020f7794ad4f401291647abf84d2bd8d608
virdze <joao.andre.freitas@hotmail.com>
2021-07-08 15:28:05 +0100

    G1Ex1

--- Guiao1/mycp.c
+++ Guiao1/mycp.c
@@ -3,7 +3,30 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(char argc, char **argv){
-    int res = open(argv[1],O_CREAT | O_TRUNC | O_RDWR, 0600);
-    for()
+#define MAXBUFFERSIZE 1024
+
+
+int main(char argc, char *argv[]) {
+    int fd_source, fd_dest;
+    ssize_t bytes_read;
+
+
+    if ((fd_source = open(argv[1], O_RDONLY)) < 0) {
+        perror("open");
+        return -1;
+    }
+
+    if ((fd_dest = open(argv[1], O_WRONLY, O_RDONLY, O_CREAT, 0640)) < 0) {
+        perror("open");
+        return -1;
+    }
+    char buffer[MAXBUFFERSIZE];
+    while ((bytes_read = read(fd_source, buffer, MAXBUFFERSIZE)) > 0) {
+        write(fd_dest, buffer, bytes_read);
+    }
+
+    close(fd_source);
+    close(fd_dest);
+
+    return 0;
 }
