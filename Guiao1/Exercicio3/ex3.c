8e8e51d3b257f2f434616d142f9d8361a518f9d6
virdze <joao.andre.freitas@hotmail.com>
2021-07-09 10:54:22 +0100

    Ex3 finished

--- Guiao1/Exercicio3/ex3.c
+++ Guiao1/Exercicio3/ex3.c
@@ -8,19 +8,25 @@ ssize_t readln(int fd, char*line, size_t size){
     ssize_t i = 0;
     ssize_t bytes_read;
 
-    while(i < size && (bytes_read = read(fd, line, 1)) > 0){
+    while(i < size && (bytes_read = read(fd, line, 1)) > 0 && line[i] != '\n')
         i += bytes_read;
-        if(line[i] == '\n') break;
-    }
     return i;
 }
 
-int main() {
+int main(char argc, char *argv[]) {
+    int fd_dest;
+
+    if((fd_dest = open(argv[1], O_WRONLY, O_RDONLY, O_CREAT, 0640)) < 0){
+        perror("open destination error");
+        return -1;
+    }
+
+
     char c;
     ssize_t bytes_read = 0;
 
     while ((bytes_read = readln(0, &c, 1)) > 0) {
-        write(1, &c, sizeof(char));
+        write(fd_dest, &c, sizeof(char));
     }
 
     return 0;
