d05856b957f9a0b52861ddf56a0fc2cf8737a7ab
virdze <joao.andre.freitas@hotmail.com>
2021-07-16 22:47:47 +0100

    Exercicios 1-5 Guiao 3

--- Guiao3/tmp.c
+++ Guiao3/tmp.c
@@ -1,14 +1,107 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char** argv){
-    int ret;
-    char* exec_args [] =
+/*
+//Exercicio 1
 
-    printf("mensagem 1\n ");
+int main(int argc, char * argv[]){
 
-    //ret = execlp("ls","/bin/ls","-l",NULL);
-    ret = execv("/bin/ls",exec_args);
-    //ret = execvp("ls",exec_args);
+    pid_t pid;
+    int status;
+    execlp(argv[1],argv[2] ,NULL);
+
     return 0;
 }
+*/
+
+/*
+//Exercicio 2
+
+int main(int argc, char * argv[]){
+
+    pid_t pid;
+    int status;
+
+    if((pid = fork()) == 0){
+        execlp("ls","-l" ,NULL);
+        _exit(pid);
+    }
+    else{
+        pid_t terminated_pid = wait(&status);
+        printf("Pai (%d) terminou filho (%d)\n", getpid(), terminated_pid);
+    }
+
+    return 0;
+}
+
+*/
+/*
+
+//Exercicio 3
+
+int main(int argc, char * argv[]){
+    printf("Argumentos recebidos:\n");
+
+    for(int i = 1 ; i < strlen(*argv) - 2; i++){
+        printf("%s ",argv[i]);
+    }
+    printf("\n");
+    return 0;
+}
+*/
+
+/*
+//Exercicio 4
+
+int main(int argc, char * argv[]) {
+    strcpy(argv[0],"ex2");
+    int ret = execv("ex2",argv);
+    return 0;
+}
+
+*/
+
+
+//Exercicio 5
+
+int main(int argc, char** argv){
+
+    int fork_ret, exec_ret;
+
+    for(int i = 1; i < argc; i++){
+        fork_ret = fork();
+        if(fork_ret == 0){
+            printf("Processom filho %d executa %s.\n",getpid(),argv[i]);
+            exec_ret = execlp(argv[i],argv[i],NULL);
+            perror("Reached error");
+            _exit(exec_ret);
+        }
+    }
+    for(int i = 1; i< argc; i++){
+        printf("Sou o pai a espera de filhos.\n");
+        wait(NULL);
+    }
+
+    printf("Todos os filhos terminaram\n");
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
